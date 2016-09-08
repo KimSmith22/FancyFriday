fe67d06c5cbeed188184a06a757dd7746f5b069a
Kimberli Smith <kimsmith.it@gmail.com>
2016-09-08 11:43:00 -0400

    Gettin user input

--- src/mainApp.java
+++ src/mainApp.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 /**
+this is a demo app to decide what to wear
  * 
  */
 
@@ -12,8 +15,26 @@ public class mainApp {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String inputFancyFriday, inputSunny, inputRaining;
+		boolean fancyFriday, sunny, raining;
+		// get input from user
+			Scanner k = new Scanner(System.in);
+			System.out.println("Is it fancy friday?");
+			inputFancyFriday = k.nextLine();
+			
+			System.out.println("Is it sunny?");
+			inputSunny = k.nextLine();
+			
+			System.out.println("Is it raining?");
+			inputRaining = k.nextLine();
+			
+			//test if the variables are working properly
+			//System.out.println(inputFancyFriday + " " + inputSunny + " " + inputRaining);
+			
+		
+		// set condition variables based on input
+		
+		// use if statement to decide what to wear
 	}
 
 }
